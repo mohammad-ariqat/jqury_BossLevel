2957cc4fccd444e8c4321970800cca0be2fddd9e
mohammad-ariqat <107495343+mohammad-ariqat@users.noreply.github.com>
2024-01-08 23:13:48 +0300

    Update game.js
    i changed the event that starts the game to click to unable phone users to play

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var buttonColours =["red", "blue", "green", "yellow"];
 var level = 0;
 
 var first = true;
-$(document).keypress(function start() {
+$(document).click(function start() {
     if (first == true) {
         $("h1").text("Level " + level);
         nextSequence();
